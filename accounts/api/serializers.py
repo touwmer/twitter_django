41d8f50c8120b6cc844de353865a9342e95c8f6b
vagrant <vagrant@vagrant.vm>
2022-10-08 22:55:58 +0000

    Login, Login Status, Logout, Signup API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,46 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['username', 'email']
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    # will be called if is_valid is called
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user
